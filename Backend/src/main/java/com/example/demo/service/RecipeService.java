9af8afa34fa3387bc3c4459f1139256149ef2bf3
kmkcc2 <kmk.krowicki@gmail.com>
2023-06-23 21:51:59 +0200

    added response for recipe with ingredients

--- Backend/src/main/java/com/example/demo/service/RecipeService.java
+++ Backend/src/main/java/com/example/demo/service/RecipeService.java
@@ -1,31 +1,61 @@
 package com.example.demo.service;
 
+import com.example.demo.entity.Ingredient;
 import com.example.demo.entity.Recipe;
+import com.example.demo.repository.IngredientRepository;
+import com.example.demo.repository.ProductRepository;
 import com.example.demo.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/recipe")
 public class RecipeService {
     @Autowired
-    private RecipeRepository repository;
+    private RecipeRepository recipeRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private IngredientRepository ingredientRepository;
 
     @GetMapping
     public Iterable<Recipe> getProducts() {
-        return repository.findAll();
+        return recipeRepository.findAll();
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Recipe> getRecipeById(@PathVariable(value = "id") int id) {
-        Optional<Recipe> recipe = repository.findById(id);
-
+    public ResponseEntity getRecipeById(@PathVariable(value = "id") int id) {
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        HashMap response = new HashMap<>();
+        HashMap ingredients = new HashMap();
         if (recipe.isPresent()) {
-            return ResponseEntity.ok().body(recipe.get());
+
+            int recipe_id = recipe.get().getId();
+            HashMap singleIngredient;
+            Iterable<Ingredient> igredients = ingredientRepository.findAll();
+            for (Ingredient ingredient : igredients) {
+                singleIngredient = new HashMap();
+                if (ingredient.getRecipe() != null) {
+                    if (ingredient.getRecipe().getId() == recipe_id) {
+                        singleIngredient.put("name", ingredient.getProduct().getName());
+                        singleIngredient.put("quantity", ingredient.getQuantity());
+                        ingredients.put("ingredient" + ingredient.getId(), singleIngredient);
+                    }
+                }
+            }
+            response.put("recipe_id", recipe_id);
+            response.put("title", recipe.get().getTitle());
+            response.put("description", recipe.get().getDescription());
+            response.put("ingredients", ingredients);
+
+
+            return ResponseEntity.ok().body(response);
         } else {
             return ResponseEntity.notFound().build();
         }
@@ -33,31 +63,31 @@ public class RecipeService {
 
     @PostMapping
     public Recipe addRecipe(@Validated @RequestBody Recipe recipe) {
-        return repository.save(recipe);
+        return recipeRepository.save(recipe);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity deleteRecipe(@PathVariable(value = "id") int id) {
-        Recipe recipe = repository.findById(id).orElse(null);
+        Recipe recipe = recipeRepository.findById(id).orElse(null);
         if (recipe == null) {
             return ResponseEntity.notFound().build();
         } else {
-            repository.delete(recipe);
+            recipeRepository.delete(recipe);
             return ResponseEntity.noContent().build();
         }
     }
 
     @PutMapping("/{id}")
     public ResponseEntity putRecipe(@PathVariable(value = "id") int id, @Validated @RequestBody Recipe newRecipe) {
-        repository.findById(id)
+        recipeRepository.findById(id)
                 .map(recipe -> {
                     recipe.setDescription(newRecipe.getDescription());
                     recipe.setTitle(newRecipe.getTitle());
-                    return repository.save(recipe);
+                    return recipeRepository.save(recipe);
                 })
                 .orElseGet(() -> {
                     newRecipe.setId(id);
-                    return repository.save(newRecipe);
+                    return recipeRepository.save(newRecipe);
                 });
         return ResponseEntity.ok().body(newRecipe);
     }
